2f909e887eb11ab07b862a1d20417980c903dd8a
Matheus Magnago <magnagomatheus7@gmail.com>
2023-10-13 15:20:24 -0300

    Posiçãoo

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+using tabuleiro;
+
+namespace xadrez_console {
+    class Program {
+        static void Main() {
+
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine($"Posição: {P}");
+
+        }
+    }
+}
---
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace tabuleiro
+{
+    internal class Posicao
+    {
+
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        public Posicao(int l, int c) {
+            this.linha = l;
+            this.coluna = c;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{linha}, {coluna}";
+        }
+    }
+}
