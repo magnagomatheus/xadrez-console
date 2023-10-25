d2703d08b9e99a42f27d63f668befc4e36b90dd1
Matheus Magnago <magnagomatheus7@gmail.com>
2023-10-25 20:29:08 -0300

    Upgrade função imprimirTabuleiro e cores para diferenciar as peças.

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -10,6 +10,7 @@ namespace xadrez_console
         public static void imprimirTabuleiro(Tabuleiro tab) {
 
             for (int i = 0; i < tab.linhas; i++) {
+                Console.Write($"{8-i} ");
                 for (int j = 0; j < tab.colunas; j++) {
 
                     if (tab.peca(i, j) == null)
@@ -17,12 +18,28 @@ namespace xadrez_console
                         Console.Write($"- ");
                     }
                     else {
-                        Console.Write($"{tab.peca(i, j)} ");
+                        imprimeirPeca(tab.peca(i, j));
+                        Console.Write($" ");
                     }
                 }
                 Console.WriteLine();
             }
 
+            Console.WriteLine($" a b c d e f g h");
+
+        }
+
+        public static void imprimeirPeca(Peca peca) {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
         }
 
     }
